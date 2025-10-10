4a1483d51829003e0c626bd1baa87a51a5d34c34
Alex Thummalapalli <alex.thummas66@gmail.com>
2025-10-09 22:56:35 -0400

    update case for typescript/javascript

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
                 <span className="font-bold text-white">Alex Thummalapalli. </span>
                 <span className="block sm:inline">CS Student.</span>
             </h1>
-            <p className="text-zinc-400 text-lg sm:text-xl mt-8"><span className="text-zinc-300 font-bold">Hi, I'm Alex!</span> I'm majoring in computer science at the Georgia Institute of Technology. I enjoy building websites using React, Typescript, HTML, and CSS. I also specialize in UI and UX design.</p>
+            <p className="text-zinc-400 text-lg sm:text-xl mt-8"><span className="text-zinc-300 font-bold">Hi, I'm Alex!</span> I'm majoring in computer science at the Georgia Institute of Technology. I enjoy building websites using React, TypeScript, HTML, and CSS. I also specialize in UI and UX design.</p>
             <div className="text-zinc-400 text-xl sm:text-2xl flex flex-col gap-4 mt-8">
                 <p><span className="font-bold text-white">Resume:</span> <a className="hover:underline" href="https://drive.google.com/file/d/1WmNBNW5tS6ZugPjRx6H2PEglHoviP7r1/view?usp=sharing" target="_blank" rel="noopener noreferrer">/resume.pdf</a></p>
                 <p><span className="font-bold text-white">Github:</span> <a className="hover:underline" href="https://github.com/alext101" target="_blank" rel="noopener noreferrer">/alext101</a></p>
