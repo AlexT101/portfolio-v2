833c3935e7fc30a62fec952dbf501e4456716b40
Alex Thummalapalli <alex.thummas66@gmail.com>
2026-07-11 09:50:28 -0400

    Add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import next from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-config-prettier";
 
 /** @type {import("eslint").Linter.Config[]} */
 const eslintConfig = [
   ...next,
+  prettier,
 ];
 
 export default eslintConfig;
